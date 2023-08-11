67ff80be2a611bdd15730a6247991e8e0696a2f6
Irina Tretiakova <iri54696376@gmail.com>
2023-08-11 22:29:04 +0200

    test script in PageObject format, works together with files in pages folder

---
+++ test_main_page2.py
@@ -0,0 +1,17 @@
+import pytest
+#import conftest
+from selenium import webdriver
+from .pages.main_page import MainPage
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+#def go_to_login_page(browser):
+#    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+#    login_link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
