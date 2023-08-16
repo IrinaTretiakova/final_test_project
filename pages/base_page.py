39b150456deefa4ab544481b1dfced684e47ded4
Irina Tretiakova <iri54696376@gmail.com>
2023-08-16 14:46:00 +0200

    In BasePage added implicitly_wait and method is_element_presentto catch exception

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
     def __init__(self, browser, url):
@@ -12,3 +12,13 @@ class BasePage():
         self.url = url
     def open(self):
         self.browser.get(self.url)
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
