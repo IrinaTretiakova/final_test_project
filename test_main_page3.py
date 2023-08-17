12c965ce37de1074c11ddaad155e5814e5606c32
Irina Tretiakova <iri54696376@gmail.com>
2023-08-17 13:56:20 +0200

    both tests are presented

--- test_main_page3.py
+++ test_main_page3.py
@@ -7,9 +7,16 @@ from selenium.webdriver.support.ui import Select
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser): 
+    #link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    #link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
