b7ededf4518a7ad74cdfbaad8be736f2ee60420a
Irina Tretiakova <iri54696376@gmail.com>
2023-08-18 17:30:25 +0200

    Imported LoginPage; Added 3 tests for Registr, Login, Url; Saving current url and adding it to last three tests

--- test_main_page3.py
+++ test_main_page3.py
@@ -1,11 +1,12 @@
 import pytest
 #import conftest
 from selenium import webdriver
-from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
 from selenium.webdriver.support.ui import WebDriverWait
+from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -14,9 +15,31 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page() 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page
 
 def test_guest_should_see_login_link(browser):
-    #link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_should_see_register_form(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_register_form()
+
+def test_guest_should_see_login_form(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
+
+def test_should_be_login_url(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
